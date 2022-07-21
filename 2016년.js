392850e3677b72866c4b974b13dc04a94c63cb05
jinaSE0 <109025674+jinaSE0@users.noreply.github.com>
2022-07-21 17:38:13 +0900

    Update 2016년.js
    추가

--- 2016년.js
+++ 2016년.js
@@ -9,3 +9,7 @@ function solution(a, b) {
     answer = day[cnt%7]
     return answer;
 }
+
+// function solution(a, b) {
+//     return new Date(2016, a-1, b).toString().slice(0,3).toUpperCase();
+// }
