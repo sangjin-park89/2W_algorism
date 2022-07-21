b9efc08d446a7379cbbf7154ce9a354c07c25c7f
jinaSE0 <109025674+jinaSE0@users.noreply.github.com>
2022-07-21 17:44:53 +0900

    Create 서울에서 김서방 찾기.js
    new

---
+++ 서울에서 김서방 찾기.js
@@ -0,0 +1,10 @@
+function solution(seoul) {
+    var answer = '';
+    
+    //킴의 위치찾기 indexof
+    let a = seoul.indexOf('Kim')
+    
+    // '김서방은'+ +'에있다'
+    answer = '김서방은 '+ a +'에 있다'
+    return answer;
+}
